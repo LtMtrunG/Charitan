e298253107e46c666cd55f4d47ea9e1fe56e6fd4
TMT <105305209+LtMtrunG@users.noreply.github.com>
2024-10-28 15:39:24 +0700

    Init spring boot

---
+++ src/main/java/com/example/Chartian/ChartianApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chartian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Chartian/ChartianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chartian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
